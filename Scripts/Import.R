a78c8ad9324da4f0913f9af99129a6f9f7fc5646
Félix Boudry <felix.boudry@univ-perp.fr>
2023-07-21 10:08:38 +0200

    Added "Data_raw" to ignored folder for git. Removed packages used only in "function.R" script and called them directly instead of loading at beginning. Added parameters declaration in Rmd file. Import script isn't needed anymore, it can be used to compute a summary file that will be saved and directly loaded for the analysis. Analysis only work for one type of data.

--- Scripts/Import.R
+++ Scripts/Import.R
@@ -3,14 +3,48 @@
 # Author: Félix Boudry
 # Contact: <felix.boudry@univ-perp.fr>
 # License: GPLv3
-# Description: Process used to import data and transforme them to be used in this study
+# Description: Process used to import data and transform them to be used in
+# this study
 
-imported_data <- project_import(project_path = "Data")
+# Libraries ---------------------------------------------------------------
+require(dplyr)
+require(tibble)
+require(FactoMineR)
+require(missMDA)
 
-my_data <- imported_data %$%
-  my_data %>%
+imported_data <- project_import(project_path = "Data_raw")
+
+infos <- imported_data$infos
+my_data <- imported_data$data %>%
   common_col()
 
-infos <- imported_data$my_data_infos
+my_colnames <- colnames(my_data[[1]])
 
 summary_simple <- my_summary(my_data, my_data)
+summary_relative <- compute_relative(summary_simple)
+
+keeped_rows <- summary_simple %>%
+  as.data.frame() %>%
+  select_if(is.numeric) %>%
+  na.omit() %>%
+  rownames()
+
+PCA_summary <- lapply(my_data[keeped_rows %>% as.numeric()], imputePCA) %>%
+  lapply(FUN = as.data.frame) %>%
+  lapply(FUN = select, contains(my_colnames)) %>%
+  lapply(FUN = PCA, graph = FALSE) %>%
+  lapply(FUN = "[", "eig") %>%
+  lapply(FUN = unlist) %>%
+  do.call(what = rbind, args = .) %>%
+  as.data.frame() %>%
+  rownames_to_column(.data = ., var = "subject")
+
+
+# Cleaning ----------------------------------------------------------------
+# Removes outliers in summaries
+my_summaries <-
+  lst(summary_simple, summary_relative, PCA_summary) %>%
+  `names<-`(value = c("absolute", "relative", "PCA"))
+
+# Labelling ---------------------------------------------------------------
+encoded_summaries <- lapply(X = my_summaries, FUN = df_encode)
